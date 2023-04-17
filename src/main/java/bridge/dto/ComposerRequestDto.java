530c14cb9b09c39b4ba4a937b6db232e2e45b60a
zgsda <121409809+zgsda@users.noreply.github.com>
2023-04-17 18:19:38 +0900

    0417_jimin

---
+++ src/main/java/bridge/dto/ComposerRequestDto.java
@@ -0,0 +1,21 @@
+package bridge.dto;
+
+import lombok.Data;
+
+@Data
+public class ComposerRequestDto {
+	 	private String userId;
+	 	private String userNickname;
+	 	private int crtidx;
+	 	private String crtTag;
+	 	private int crIdx;
+	 	private int pdIdx;
+	 	private String userId1;
+	 	private String userId2;
+	 	private int pdMoney;
+	 	private String pdcComment;
+	 	private int pdcIdx;
+	 	private int plMoney;
+	 	private String plDate;
+	
+}
