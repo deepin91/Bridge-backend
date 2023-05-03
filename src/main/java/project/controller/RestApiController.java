efeeea340641b235852d9011e7a3083f27641869
Sihyun3 <121409925+Sihyun3@users.noreply.github.com>
2023-05-03 11:39:39 +0900

    Delete src/main/java/project directory

--- src/main/java/project/controller/RestApiController.java
+++
@@ -1,9 +0,0 @@
-package project.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-//@CrossOrigin(origins = "*", allowedHeaders = "*",allowedMethods = "*")
-public class RestApiController {
-
-}
--- src/main/java/project/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package project.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-	private String userId;
-	private String userPw;
-}
